70c1b117decf618924a02efc4082ad1498b72d1a
imvm <imvm@cin.ufpe.br>
2016-09-27 16:00:56 -0300

    created xcode project

---
+++ Repositorio/Hub-APS/Hub-APS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hub-APS
+//
+//  Created by Ian Manor on 9/27/16.
+//  Copyright © 2016 aps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Repositorio/Hub-APS/Hub-APS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hub-APS
+//
+//  Created by Ian Manor on 9/27/16.
+//  Copyright © 2016 aps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
