99a0352b67fedbdf708c734f53cf7de1102cbfe2
Hilton Pintor <hpbl@cin.ufpe.br>
2016-09-27 17:40:08 -0300

    Criando metodos das entidades

--- Repositorio/hubAPS/hubAPS/Troca.swift
+++ Repositorio/hubAPS/hubAPS/Troca.swift
@@ -10,5 +10,21 @@ import Foundation
 
 class Troca {
     
+    var remetente : Conta
+    var destinatario : Conta
+    var remetenteItem : Item
+    var destinatarioItem : Item
+    
+    init(remetente: Conta, destinatario: Conta, remetenteItem: Item, destinatarioItem : Item) {
+        self.remetente = remetente
+        self.destinatario = destinatario
+        self.remetenteItem = remetenteItem
+        self.destinatarioItem = destinatarioItem
+    }
+    
+    func troca(remetente: Conta, destinatario: Conta, remetenteItem: Item, destinatarioItem : Item) -> Conta {
+        return troca(remetente: remetente, destinatario: destinatario, remetenteItem: remetenteItem, destinatarioItem: destinatarioItem)
+        
+    }
     
 }
