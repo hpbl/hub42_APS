f0e4245245c78a70a3e6a1afefb6236042997dac
Hilton Pintor <hpbl@cin.ufpe.br>
2016-09-27 18:21:43 -0300

    Criando protocolos dos repositorios

---
+++ Repositorio/hubAPS/hubAPS/IRepositorioItens.swift
@@ -0,0 +1,16 @@
+//
+//  IRepositorioItens.swift
+//  hubAPS
+//
+//  Created by Hilton Pintor Bezerra Leite on 27/09/16.
+//  Copyright © 2016 Hilton Pintor Bezerra Leite. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+protocol IRepositorioItens {
+    
+    func inserirItem(item: Item)
+    func solicitarItensPerto(localizacao: CLLocation)
+}
