654427afc23711bc1a9f5644ef5c8c4e87f31c7a
Hilton Pintor <hpbl@cin.ufpe.br>
2016-10-02 14:28:27 -0300

    criando config.plist e NSUserDefaults

---
+++ Repositorio/hubAPS copy/hubAPS/UserInfo.swift
@@ -0,0 +1,25 @@
+//
+//  UserInfo.swift
+//  hubAPS
+//
+//  Created by Hilton Pintor Bezerra Leite on 29/09/16.
+//  Copyright © 2016 Hilton Pintor Bezerra Leite. All rights reserved.
+//
+
+import Foundation
+
+class UserInfo {
+    
+    var idUsuario: String? {
+        get {
+            let returnValue = UserDefaults.standard.string(forKey: "idUsuario")
+                return returnValue
+
+            
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: "idUsuario")
+            UserDefaults.standard.synchronize()
+        }
+    }
+}
